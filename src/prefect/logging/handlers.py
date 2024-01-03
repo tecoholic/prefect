3dcb8cfe4a9c3759c3437d3a78a106ac76f389c1
zangell44 <zachary.james.angell@gmail.com>
2024-01-03 12:49:46 -0500

    Fix logging handler

--- src/prefect/logging/handlers.py
+++ src/prefect/logging/handlers.py
@@ -1,14 +1,13 @@
+import datetime
 import json
 import logging
 import sys
-import time
 import traceback
 import uuid
 import warnings
 from contextlib import asynccontextmanager
 from typing import Any, Dict, List, Type, Union
 
-import pendulum
 from rich.console import Console
 from rich.highlighter import Highlighter, NullHighlighter
 from rich.theme import Theme
@@ -220,14 +219,18 @@ class APILogHandler(logging.Handler):
         except ValueError:
             is_uuid_like = False
 
+        created_time = getattr(record, "created", None)
+        timestamp = (
+            datetime.datetime.fromtimestamp(created_time)
+            if created_time is not None
+            else datetime.datetime.now(tz=datetime.timezone.utc)
+        )
         log = LogCreate(
             flow_run_id=flow_run_id if is_uuid_like else None,
             task_run_id=task_run_id,
             name=record.name,
             level=record.levelno,
-            timestamp=pendulum.from_timestamp(
-                getattr(record, "created", None) or time.time()
-            ),
+            timestamp=timestamp,
             message=self.format(record),
         ).dict(json_compatible=True)
 
